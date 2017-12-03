1ee6c1c9807592ce8dc5340c4a58bc7d20b12d12
Vladimir Mikhalev <vladimirmsm@yandex.ru>
2017-12-03 10:36:01 +0300

    change screen animation

--- app/src/main/java/io/github/vladimirmi/radius/presentation/root/RootActivity.kt
+++ app/src/main/java/io/github/vladimirmi/radius/presentation/root/RootActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.support.annotation.StringRes
-import android.support.v4.app.Fragment
 import android.support.v7.view.menu.MenuBuilder
 import android.support.v7.view.menu.MenuPopupHelper
 import android.support.v7.widget.PopupMenu
@@ -17,17 +16,11 @@ import com.arellomobile.mvp.MvpAppCompatActivity
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
 import io.github.vladimirmi.radius.R
-import io.github.vladimirmi.radius.Screens
 import io.github.vladimirmi.radius.di.Scopes
 import io.github.vladimirmi.radius.di.module.RootActivityModule
-import io.github.vladimirmi.radius.model.entity.Station
-import io.github.vladimirmi.radius.presentation.mediaList.MediaListFragment
-import io.github.vladimirmi.radius.presentation.station.StationFragment
 import kotlinx.android.synthetic.main.activity_root.*
 import kotlinx.android.synthetic.main.view_menu_item.view.*
 import ru.terrakok.cicerone.NavigatorHolder
-import ru.terrakok.cicerone.android.SupportAppNavigator
-import ru.terrakok.cicerone.commands.Command
 import toothpick.Toothpick
 import javax.inject.Inject
 
@@ -40,6 +33,7 @@ class RootActivity : MvpAppCompatActivity(), RootView, ToolbarView {
     @Inject lateinit var navigatorHolder: NavigatorHolder
     @InjectPresenter lateinit var presenter: RootPresenter
 
+    private val navigator = Navigator(this, R.id.fragment_container)
     private val toolBarMenuItems = ArrayList<MenuItemHolder>()
 
     @ProvidePresenter
@@ -77,36 +71,6 @@ class RootActivity : MvpAppCompatActivity(), RootView, ToolbarView {
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         presenter.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
-
-    //todo Standalone class
-    private val navigator = object : SupportAppNavigator(this, R.id.fragment_container) {
-
-        private var currentKey: String? = null
-
-        override fun createActivityIntent(screenKey: String, data: Any?) = null
-
-        override fun createFragment(screenKey: String, data: Any?): Fragment? {
-            if (currentKey == screenKey) return null
-            return when (screenKey) {
-                Screens.MEDIA_LIST_SCREEN -> MediaListFragment()
-                Screens.STATION_SCREEN -> StationFragment.newInstance(data as Station)
-                else -> null
-            }
-        }
-
-        override fun applyCommand(command: Command?) {
-            currentKey = with(supportFragmentManager) {
-                if (backStackEntryCount > 0) {
-                    getBackStackEntryAt(backStackEntryCount - 1)?.name
-                } else null
-            }
-            super.applyCommand(command)
-        }
-
-        override fun unknownScreen(command: Command?) {
-            //do nothing
-        }
     }
 
     //region =============== RootView ==============

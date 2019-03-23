aa8b44ced258e6e2d0023ffe2f4e0b32def73134
Vladimir Mikhalev <vladimirmsm@yandex.ru>
2019-03-23 18:08:26 +0300

    feat: Navigation animation

--- app/src/main/java/io/github/vladimirmi/internetradioplayer/presentation/navigation/NavigationHolderFragment.kt
+++ app/src/main/java/io/github/vladimirmi/internetradioplayer/presentation/navigation/NavigationHolderFragment.kt
@@ -1,10 +1,15 @@
 package io.github.vladimirmi.internetradioplayer.presentation.navigation
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator
+import androidx.transition.Slide
+import androidx.transition.TransitionManager
 import io.github.vladimirmi.internetradioplayer.R
 import io.github.vladimirmi.internetradioplayer.extensions.visible
 import io.github.vladimirmi.internetradioplayer.navigation.ScreenContext
@@ -38,26 +43,40 @@ abstract class NavigationHolderFragment : Fragment(), BackPressListener {
                 || backTo(currentScreenContext.parent)
     }
 
-    private fun setupNavigation(screenContext: ScreenContext) {
-        if (screenContext.parent != null) {
-            parentBt.setOnClickListener { backTo(screenContext.parent) }
-            parentBt.text = screenContext.parent.title
-        }
-        screenTitleTv.text = screenContext.title
-        navigationContainer.visible(screenContext.parent != null, gone = false)
-
-        val inflater = LayoutInflater.from(requireContext())
-        childrenContainer.removeAllViews()
-        screenContext.children.forEachIndexed { index, childContext ->
-            val childView = childContext.createSmallView(inflater, childrenContainer)
-            childView.setOnClickListener { navigateTo(childContext) }
-            childrenContainer.addView(childView)
-        }
+    private fun setupNavigation(screenContext: ScreenContext, forward: Boolean) {
+        setupParent(screenContext, forward)
+        setupChildren(screenContext, forward)
         currentScreenContext = screenContext
     }
 
+    private fun setupParent(screenContext: ScreenContext, forward: Boolean) {
+        if (screenContext.parent != null) {
+            parentBt.setOnClickListener { backTo(screenContext.parent) }
+            animateParent(forward) {
+                parentTitleTv.text = screenContext.parent.title
+                screenTitleTv.text = screenContext.title
+            }
+        }
+        navigationContainer.visible(screenContext.parent != null, gone = false)
+    }
+
+    private fun setupChildren(screenContext: ScreenContext, forward: Boolean) {
+        val inflater = LayoutInflater.from(requireContext())
+        val container = LinearLayout(requireContext()).apply {
+            orientation = LinearLayout.VERTICAL
+        }
+        screenContext.children.forEach { child ->
+            val view = child.createSmallView(inflater, childrenContainer)
+            view.setOnClickListener { navigateTo(child) }
+            container.addView(view)
+        }
+        animateChildren(container, forward)
+        childrenContainer.removeAllViews()
+        childrenContainer.addView(container)
+    }
+
     private fun navigateTo(screenContext: ScreenContext) {
-        setupNavigation(screenContext)
+        setupNavigation(screenContext, forward = true)
         val fragment = screenContext.createFragment()
         fragment.userVisibleHint = userVisibleHint
         childFragmentManager.beginTransaction()
@@ -68,8 +87,45 @@ abstract class NavigationHolderFragment : Fragment(), BackPressListener {
 
     private fun backTo(screenContext: ScreenContext?): Boolean {
         if (screenContext == null) return false
-        setupNavigation(screenContext)
+        setupNavigation(screenContext, forward = false)
         childFragmentManager.popBackStack()
         return true
+    }
+
+    private fun animateChildren(target: View, forward: Boolean) {
+        val slideIn = Slide()
+                .apply { slideEdge = if (forward) Gravity.END else Gravity.START }
+                .setInterpolator(FastOutSlowInInterpolator())
+        slideIn.addTarget(target)
+
+        TransitionManager.beginDelayedTransition(childrenContainer, slideIn)
+    }
+
+    private fun animateParent(forward: Boolean, onEnd: () -> Unit) {
+        if (forward) {
+            val startX = screenTitleTv.x
+            val endX = parentTitleTv.x
+            parentTitleTv.visible(false)
+            screenTitleTv.animate()
+                    .setInterpolator(FastOutSlowInInterpolator())
+                    .x(endX)
+                    .withEndAction {
+                        screenTitleTv.x = startX
+                        parentTitleTv.visible(true)
+                        onEnd()
+                    }
+        } else {
+            val startX = parentTitleTv.x
+            val endX = (requireView().width - parentTitleTv.width).toFloat()
+            screenTitleTv.visible(false)
+            parentTitleTv.animate()
+                    .setInterpolator(FastOutSlowInInterpolator())
+                    .x(endX)
+                    .withEndAction {
+                        parentTitleTv.x = startX
+                        screenTitleTv.visible(true)
+                        onEnd()
+                    }
+        }
     }
 }
